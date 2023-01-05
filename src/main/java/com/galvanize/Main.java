37ea1701769731b49fc6e11f801ee7a178de0fb3
kelemii <98933496+kelemii@users.noreply.github.com>
2023-01-05 08:34:35 -0800

    Initialized

---
+++ src/main/java/com/galvanize/Main.java
@@ -0,0 +1,7 @@
+package com.galvanize;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(String.format("Name: %s Email: %s", args[0], args[1]));
+    }
+}
